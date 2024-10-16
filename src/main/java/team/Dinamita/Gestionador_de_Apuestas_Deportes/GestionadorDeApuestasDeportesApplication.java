5e806eddeec030b0ca72c322e8fb6f23d1265e66
Juan <juan@example.com>
2024-10-15 19:06:04 -0500

    Actualizacion depedencias

---
+++ src/main/java/team/Dinamita/Gestionador_de_Apuestas_Deportes/GestionadorDeApuestasDeportesApplication.java
@@ -0,0 +1,13 @@
+package team.Dinamita.Gestionador_de_Apuestas_Deportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionadorDeApuestasDeportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionadorDeApuestasDeportesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/Dinamita/Gestionador_de_Apuestas_Deportes/GestionadorDeApuestasDeportesApplicationTests.java
@@ -0,0 +1,13 @@
+package team.Dinamita.Gestionador_de_Apuestas_Deportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionadorDeApuestasDeportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
