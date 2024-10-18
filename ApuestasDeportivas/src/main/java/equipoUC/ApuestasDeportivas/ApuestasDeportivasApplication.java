9f5345a0e31a8f77aa514b9e36551b197dc80e22
juandax777 <jrozof@ucentral.edu.co>
2024-10-17 19:30:49 -0500

    Dependencias2
    Agregar por bugs con las dependencias anteriores

---
+++ ApuestasDeportivas/src/main/java/equipoUC/ApuestasDeportivas/ApuestasDeportivasApplication.java
@@ -0,0 +1,13 @@
+package equipoUC.ApuestasDeportivas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApuestasDeportivasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApuestasDeportivasApplication.class, args);
+	}
+
+}
