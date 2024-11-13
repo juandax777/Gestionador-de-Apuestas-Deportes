27428573e4cc57e3f0dd03d73a0fc687802d5ccf
Johandc15 <169013693+Johandc15@users.noreply.github.com>
2024-11-12 19:12:11 -0500

    Correcion de la rama version2

--- Gestionador-de-Apuestas-Deportes-CU06/Gestionador-de-Apuestas-Deportes-desarrollos/src/main/java/com/uc/ApuestasDeportivas/Persistencia/Repositorios/ApuestaRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.uc.ApuestasDeportivas.Persistencia.Repositorios;
-
-import com.uc.ApuestasDeportivas.Persistencia.Entidades.Apuesta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ApuestaRepositorio extends JpaRepository<Apuesta, Long> {
-}
--- Gestionador-de-Apuestas-Deportes-CU06/Gestionador-de-Apuestas-Deportes-desarrollos/src/main/java/com/uc/ApuestasDeportivas/Persistencia/Repositorios/TransaccionRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.uc.ApuestasDeportivas.Persistencia.Repositorios;
-
-import com.uc.ApuestasDeportivas.Persistencia.Entidades.Transaccion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransaccionRepositorio extends JpaRepository<Transaccion, Long> {
-}
